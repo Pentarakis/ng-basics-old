3ea7629b1f94b742469873a536314933dfc0f6df
Konstantin Pentarakis <konstantin.pentarakis@adesso.de>
2019-03-03 20:18:20 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'got-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ng-basics';
+}
