fcdaea3f6c89fe9d51aebc9fa51496c5dafd7c40
Konstantin Pentarakis <konstantin.pentarakis@adesso.de>
2019-03-09 00:25:28 +0100

    11a: Richtiger Umgang mit RouterParams und Observables

--- src/app/character/character/character.component.ts
+++ src/app/character/character/character.component.ts
@@ -1,40 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Character } from '../model/character';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CharacterService } from '../services/character.service';
-import { Observable } from 'rxjs/index';
+import { Observable, Subject } from 'rxjs/index';
+import { filter, pluck, switchMap, takeUntil } from 'rxjs/internal/operators';
 
 @Component({
   selector: 'got-character',
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.css']
 })
-export class CharacterComponent implements OnInit {
+export class CharacterComponent implements OnInit, OnDestroy {
 
   selected: Character = new Character();
   isCreateMode = false;
+
+  private destroy = new Subject<boolean>();
 
   constructor(private characterService: CharacterService, private router: Router, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    const id = this.route.snapshot.params.id;
-    if (id === 'create') {
-      this.selected = new Character();
-      this.isCreateMode = true;
-    } else if (id) {
-      this.characterService.read(Number(id))
-        .subscribe((character: Character) => {
-          this.selected = character;
-        });
-    }
+
+    const id$ = this.route.params.pipe(pluck('id'));
+    id$
+      .pipe(
+        filter(id => !id),
+        takeUntil(this.destroy)
+      )
+      .subscribe(() => {
+        this.selected = new Character();
+        this.isCreateMode = true;
+      });
+    id$
+      .pipe(
+        filter(id => !!id),
+        switchMap((id: number) => this.characterService.read(Number(id))),
+        takeUntil(this.destroy)
+      )
+      .subscribe((character: Character) => {
+        this.selected = character;
+      });
   }
 
   save(): void {
-    this.createOrUpdate().subscribe(() => {
-      alert('Speichern erfoglreich!');
-    });
-    this.router.navigate([`../`], { relativeTo: this.route });
+    this.createOrUpdate()
+      .pipe(
+        takeUntil(this.destroy)
+      )
+      .subscribe(() => {
+        alert('Speichern erfoglreich!');
+        this.router.navigate([`../`], { relativeTo: this.route });
+      });
   }
 
   private createOrUpdate(): Observable<Character> {
@@ -43,5 +60,10 @@ export class CharacterComponent implements OnInit {
     } else {
       return this.characterService.update(this.selected);
     }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next(true);
+    this.destroy.complete();
   }
 }
